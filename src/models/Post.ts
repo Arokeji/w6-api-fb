ff007379e1318aa15cfcbfee582702dbdc73f23e
Humberto CL <87952396+Arokeji@users.noreply.github.com>
2023-05-27 10:26:37 +0200

    Creacion del model Post

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,76 +1,36 @@
-import validator from "validator";
-import bcrypt from "bcrypt";
-import { type IBook } from "./Group";
+import { type IGroup } from "./Group";
+import { type IUser } from "./User";
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const validCountries: string[] = ["COLOMBIA", "ENGLAND", "RUSSIA", "UNITED STATES", "ARGENTINA", "CZECHOSLOVAKIA", "JAPAN", "NIGERIA"];
-
-export interface IAuthor {
-  user: string;
-  password?: string;
-  name: string;
-  country: string;
-  books?: IBook;
+export interface IPost {
+  user: IUser;
+  text: string;
+  attachedFile: string;
+  group?: IGroup;
 }
 
 // Creacion del esquema del autor
-const authorSchema = new Schema<IAuthor>({
+const authorSchema = new Schema<IPost>({
   user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  text: {
     type: String,
     trim: true,
-    unique: true,
-    required: true,
-    validate: {
-      validator: (text: string) => validator.isEmail(text),
-      message: "El usuario debe ser un email.",
-    },
-  },
-  password: {
-    type: String,
-    trim: true,
-    minLength: [8, "La contraseña debe contener un minimo de 8 caracteres"],
-    select: false, // Indica que no se muestra en las consultas
+    minLength: [0, "No se puede enviar un post vacío."],
     required: true,
   },
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    minLength: [3, "No se ha alcanzado el minimo de caracteres"],
-    maxLength: [50, "Se ha superado el maximo de caracteres"],
-  },
-  country: {
+  attachedFile: {
     type: String,
     required: false,
-    trim: true,
-    uppercase: true,
-    enum: validCountries,
   },
-  books: {
-    type: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Book",
-    },
+  group: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
   },
-});
-
-// Encriptado de la contraseña antes de que guarde
-// Usamos function en vez de una arrow para que pueda leer el this fuera de esta
-authorSchema.pre("save", async function (next) {
-  try {
-    // Si la contraseña ya estaba encriptada no la encripta de nuevo
-    if (this.isModified("password")) {
-      const saltRounds = 10; // Dureza del ecriptado
-      const hash = await bcrypt.hash(this.password as string, saltRounds);
-      this.password = hash;
-    }
-
-    next();
-  } catch (error: any) {
-    next(error);
-  }
 });
 
 // Creacion del modelo en si con un nombre y la configuracion del esquema
-export const Author = mongoose.model<IAuthor>("Author", authorSchema);
+export const Post = mongoose.model<IPost>("Post", authorSchema);
